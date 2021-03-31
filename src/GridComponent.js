0a41ea4f279418ec6f83778c36d9c45fb0cca965
Rustam <rustam905@gmail.com>
2021-03-30 17:38:43 -0700

    start

---
+++ src/GridComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./grid.css";
+
+function GridComponent() {
+	return (
+		<div className='container'>
+			<div id='content1'>Content 1</div>
+			<div id='content2'>Content 2</div>
+			<div id='content3'>Content 3</div>
+			<div id='content4'>Content 4</div>
+			<div id='content5'>Content 5</div>
+			<div id='content6'>Content 6</div>
+		</div>
+	);
+}
+
+export default GridComponent;
